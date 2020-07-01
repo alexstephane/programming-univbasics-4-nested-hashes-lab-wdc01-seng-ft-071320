7c9955b69b333c7632f3b83ff8945e9166313133
Alex Ngundji <alexngundji@gmail.com>
2020-07-01 13:49:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -270,6 +270,11 @@ def adding_to_dennis
       }
     }
 
+     :languages => ["C"]
+        }	        }
+     }	     }
+
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
      return programmer_hash
 end
 
