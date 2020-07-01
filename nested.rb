f1e29142ffe76326b0929cc0a83ed47c79863343
Alex Ngundji <alexngundji@gmail.com>
2020-07-01 13:51:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -268,9 +268,10 @@ def adding_to_dennis
         :known_for => "Unix",
         :languages => ["C"]
       }
+      programmer_hash[:alan_kay][:known_for] = alans_new_info
     }
 
-      programmer_hash[:alan_kay][:known_for] = alans_new_info
+      
       return programmer_hash
 end
 
