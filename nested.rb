d4004bb1db983b2622ac143076f011bcfe329d3e
Alex Ngundji <alexngundji@gmail.com>
2020-07-01 13:45:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -221,7 +221,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash.merge(:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP, C"]
+ })
 end
 
 def changing_alan
